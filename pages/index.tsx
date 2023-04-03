c607f7b115d37a5bec1435c9737317bb796f3e97
Yarilo Villanueva <contact@yarilovillanueva.com>
2023-04-03 15:43:32 +0200

    Fetch & parse data from the API
    We do it client side, as as per Next.js docs, this type of data ("a dashboard")  should be retrieved here: https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props#fetching-data-on-the-client-side
    
    Also, we use `SWR` as recommended by Next.js docs (https://nextjs.org/docs/basic-features/data-fetching/client-side#client-side-data-fetching-with-swr)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,67 @@
-export default function Index() {
-  return <></>;
+import useSWR from 'swr'
+
+const ENDPOINT = '/api/rainfall';
+// @TODO: Adjust types
+
+type Row = { [headerName: string]: string };
+type Column = {
+  headerName: string;
+  key: string;
 }
+
+const fetcher = (endpoint) => fetch(endpoint).then(res => res.json())
+
+const parseDate = (date = '') => date.split('T')[0]; // As we don't need hours, minutes and so on
+function getRainByRegionAndDate(data = []) {
+  const rainByRegionAndDate = {};
+  data.forEach((entry) => {
+    const parsedDate = parseDate(entry.date);
+    entry.data.forEach(e => {
+      rainByRegionAndDate[e.regionName] = rainByRegionAndDate[e.regionName] || {};
+      rainByRegionAndDate[e.regionName][parsedDate] = e.value;
+    })
+  });
+  return rainByRegionAndDate;
+}
+
+
+
+const REGION_COLUMN: Column = {
+  headerName: 'Region',
+  key: 'regionName',
+}
+
+function getColumns(data = []): Column[] {
+  const dates: { [key: string]: string } = {};
+  for (const entry of data) {
+    const parsedDate = parseDate(entry.date);
+    dates[parsedDate] = parsedDate;
+  }
+  const DATE_COLUMNS: Column[] = Object.values(dates).map(date => {
+    return { headerName: date, key: date }
+  })
+  return [REGION_COLUMN, ...DATE_COLUMNS];
+};
+
+function getRows(data): Row[] {
+  const rainByRegionAndDate = getRainByRegionAndDate(data);
+  const rows: any[] = Object.keys(rainByRegionAndDate).map(region => {
+    const row = { regionName: region };
+    Object.entries(rainByRegionAndDate[region]).forEach(([date, rainValue]) => {
+      row[date] = rainValue;
+    })
+    return row;
+  });
+  return rows;
+}
+
+export default function Index() {
+  const { data } = useSWR(ENDPOINT, fetcher)
+  if (!data) return null;
+  const rows = getRows(data);
+  const columns = getColumns(data);
+  console.log({ rows, columns });
+  return <><p>Hi!</p></>;
+}
+
+
